f50ea57d2a51c1a919374f281e5b68b03650423c
claypooltyler00 <claypooltyler00@gmail.com>
2022-04-10 14:22:31 -0500

    creating headers

---
+++ partail_files/object_partial_files/new_user.php
@@ -0,0 +1,9 @@
+<?php
+include_once '../../models/BaseClass.php';
+include_once '../../models/User.php';
+include_once '../../config/Database.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db);
