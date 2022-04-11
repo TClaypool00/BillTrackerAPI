2a7b1054bd9a88dce67580a61b0c1b29f97366fa
claypooltyler00 <claypooltyler00@gmail.com>
2022-04-10 21:04:10 -0500

    Added more functions to global functions file

--- global_functions.php
+++ global_functions.php
@@ -2,3 +2,28 @@
 function custom_array($message) {
     return json_encode(array('message' => $message));
 }
+
+function get_isset($get_name) {
+    if (isset($_GET[$get_name])) {
+        return true;
+    }
+
+    return false;
+}
+
+function set_get_variable($get_name) {
+    return $_GET[$get_name];
+}
+
+function display_list($array) {
+    return json_encode($array);
+}
+
+function set_id() {
+    if (get_isset('id')) {
+        return set_get_variable('id');
+    } else {
+        echo 'id cannot be null';
+        die();
+    }
+}
