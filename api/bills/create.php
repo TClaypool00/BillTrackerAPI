0f60b2d5bb75c7be069e407fcdec28eae41b76e4
claypooltyler00 <claypooltyler00@gmail.com>
2022-04-11 22:54:40 -0500

    Creating a bill works

---
+++ api/bills/create.php
@@ -0,0 +1,16 @@
+<?php
+include '../../partail_files/create_header.php';
+include '../../partail_files/object_partial_files/new_bill.php';
+include '../../global_functions.php';
+
+$bill->bill_name = $data->billName;
+$bill->amount_due = $data->amountDue;
+$bill->user_id = $data->userId;
+
+if ($bill->create()) {
+    http_response_code(201);
+    echo custom_array('Bill has been created');
+} else {
+    http_response_code(400);
+    echo custom_array('bill could not be created');
+}
--- models/Bill.php
+++ models/Bill.php
@@ -8,4 +8,18 @@ class Bill extends BaseClass {
     {
         $this->conn = $db;
     }
+
+    public function create() {
+        $this->clean_data();
+
+        $this->stmt = $this->prepare_stmt("CALL insUser('{$this->bill_name}', '{$this->amount_due}', '{$this->user_id}');");
+
+        return $this->stmt_executed();
+    }
+
+    private function clean_data() {
+        $this->bill_name = htmlspecialchars(strip_tags($this->bill_name));
+        $this->amount_due = htmlentities(strip_tags($this->amount_due));
+        $this->user_id = htmlspecialchars(strip_tags($this->user_id));
+    }
 }
