b5d74bafc654e4622402e8bdd3532ee9fff04313
claypooltyler00 <claypooltyler00@gmail.com>
2022-04-11 21:58:15 -0500

    Update header

---
+++ partail_files/update_header.php
@@ -0,0 +1,7 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-Width");
+
+$data = json_decode(file_get_contents('php://input'));
