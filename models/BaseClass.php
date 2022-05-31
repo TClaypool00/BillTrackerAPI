cf7e42af6672b6f941e4d0085841fdd238178156
claypooltyler00 <claypooltyler00@gmail.com>
2022-05-30 21:32:40 -0500

    created a function to convert a number to boolean, and various other sql functions

--- models/BaseClass.php
+++ models/BaseClass.php
@@ -21,6 +21,22 @@ class BaseClass {
 
     public $date_due;
 
+    public function currency($value) {
+        return '$' . $value;
+    }
+
+    public function pay($amount, $id, $type_id) {
+        $this->stmt = $this->prepare_stmt("CALL updPayExpense('{$id}', '{$amount}', '{$type_id}');");
+
+        return $this->stmt_executed();
+    }
+
+    public function is_paid($id) {
+        $this->stmt = $this->prepare_stmt('SELECT IsPaid from paymenthistory where ExpenseId = ' . $id);
+        $this->execute();
+        return $this->convert_to_boolean($this->stmt->fetchColumn());
+    }
+
     protected function stmt_executed() {
         if ($this->stmt->execute()) {
             return true;
@@ -43,21 +59,26 @@ class BaseClass {
         $this->stmt->execute();
     }
 
-    public function currency($value) {
-        return '$' . $value;
-    }
-
-    public function pay($amount, $id, $type_id) {
-        $this->stmt = $this->prepare_stmt("CALL updPayExpense('{$id}', '{$amount}', '{$type_id}');");
-
-        return $this->stmt_executed();
-    }
-
     protected function additional_query_empty() {
         if ($this->additional_query === '' || $this->additional_query === null) {
             $this->additional_query = ' WHERE ';
         } else {
             $this->additional_query .= ' AND ';
+        }
+    }
+
+    protected function convert_to_boolean($value) {
+        if (gettype($value) === 'boolean') {
+            return $value;
+        } else if (gettype($value) === 'integer') {
+            if ($value === 0) {
+                return false;
+            } else if ($value === 1) {
+                return true;
+            }
+        } else {
+            http_response_code(400);
+            custom_array('not valid');
         }
     }
 }
