a091305ec5407af83413603e1e1af8051635acf3
claypooltyler00 <claypooltyler00@gmail.com>
2022-06-05 00:35:36 -0500

    Made token and decoded not null at first

--- partail_files/jwt_partial.php
+++ partail_files/jwt_partial.php
@@ -4,15 +4,15 @@ require '../../vendor/autoload.php';
 use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key;
 
-$token = null;
-$decoded = null;
+$token;
+$decoded;
 $headers = getallheaders();
 $token = $headers['Authorization'] ?? null;
 
 if ($token !== null) {
     try {
         $decoded = JWT::decode($token, new Key(Secret::$key, Secret::$alg));
-        $company->user_id = $decoded->userId;
+        
     } catch(Exception $e) {
         http_response_code(401);
         echo custom_array($e->getMessage());
