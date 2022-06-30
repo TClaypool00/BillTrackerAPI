7b4c2664a2f8206c0610448a32764d070648ba2a
claypooltyler00 <claypooltyler00@gmail.com>
2022-06-29 19:45:08 -0500

    Added input validation

--- api/bills/get_all.php
+++ api/bills/get_all.php
@@ -1,48 +1,89 @@
 <?php
 include '../../partail_files/get_all_header.php';
-include '../../partail_files/object_partial_files/new_bill.php';
 include '../../global_functions.php';
-
-$by_user = false;
-$by_active = false;
+include '../../partail_files/object_partial_files/new_bill.php';
+include '../../partail_files/jwt_partial.php';
 
 if (get_isset('userId')) {
     $bill->user_id = set_get_variable('userId');
-    $by_user = true;
+} else {
+    $bill->user_id = null;
 }
 
 if (get_isset('isActive')) {
     $bill->is_active = set_get_variable('isActive');
-    $by_active = true;
+} else {
+    $bill->is_active = null;
 }
 
-$result = $bill->get_all($by_user, $by_active);
+if (get_isset('companyId')) {
+    $bill->company_id = set_get_variable('companyId');
+} else {
+    $bill->company_id = null;
+}
 
-$num = $result->rowCount();
+$bill->validate_user_id(true);
+$bill->validate_is_active(true);
+$bill->validate_company_id(true);
 
-if ($num > 0) {
-    $bill_arr = array();
+if ($bill->status_is_empty()) {
+    if (!$decoded->isAdmin) {
+        if ($bill->is_active === null && $bill->user_id === null) {
+            http_response_code(403);
+            echo custom_array(Bill::$all_params_null);
+            die();
+        }
 
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+        if (!is_null($bill->user_id)) {
+            if ($decoded->userId !== $bill->user_id) {
+                http_response_code(403);
+                echo custom_array(Bill::$not_auth);
+                die();
+            }
 
-        $bill_item = array(
-            'billId' => $BillId,
-            'billName' => $BillName,
-            'amountDue' => $bill->currency($AmountDue),
-            'isRecurring' => $IsRecurring,
-            'isActive' => $IsActive,
-            'endDate' => $EndDate,
-            'userId' => $UserId,
-            'firstName' => $FirstName,
-            'lastName' => $LastName
-        );
-        array_push($bill_arr, $bill_item);
+            if (!is_null($bill->company_id) && !$bill->user_has_company()) {
+                http_response_code(403);
+                echo custom_array(Bill::$does_not_have_company);
+                die();
+            }
+        }
     }
 
-    http_response_code(200);
-    echo json_encode($bill_arr);
+    $result = $bill->get_all();
+
+    $num = $result->rowCount();
+
+    if ($num > 0) {
+        $bill_arr = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $bill_item = array(
+                'billId' => $BillId,
+                'billName' => $BillName,
+                'amountDue' => currency($AmountDue),
+                'isActive' => boolval($IsActive),
+                'dateDue' => $DateDue,
+                'datePaid' => $DatePaid,
+                'isPaid' => boolval($IsPaid),
+                'isLate' => boolval($IsLate),
+                'companyId' => $CompanyId,
+                'companyName' => $CompanyName,
+                'userId' => $UserId,
+                'firstName' => $FirstName,
+                'lastName' => $LastName
+            );
+            array_push($bill_arr, $bill_item);
+        }
+
+        http_response_code(200);
+        echo json_encode($bill_arr);
+    } else {
+        http_response_code(404);
+        echo custom_array('No bills found');
+    }
 } else {
     http_response_code(404);
-    echo custom_array('No bills found');
+    echo custom_array($bill->status);
 }
